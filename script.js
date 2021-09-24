8701f01c000d543389c99b4e95f6abeea25b9150
karenplimmerphotography <75963585+karenplimmerphotography@users.noreply.github.com>
2021-09-25 07:44:41 +1000

    animate openClose menu icon

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-const mySideNav =  document.getElementById("mySideNav");
+const mySideNav = document.getElementById("mySideNav");
 const open = document.getElementById("mobile-menu-open");
-const close = document.getElementById("mobile-menu-x");
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
+const line3 = document.getElementById("line3");
 
-function openNav() {
-  mySideNav.style.width = "250px";
-  mySideNav.style.opacity = "1";
-  }
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
+function openCloseNav() {
+  if (mySideNav.style.width === '250px') {
     mySideNav.style.width = "0";
+    line1.style.display = '';
+    line1.style.transform = ''
+    line2.style.opacity = '1';
+    line1.style.bottom = "";
+    line1.style.right = "";
+    line3.style.display = '';
+    line3.style.transform = '';
+    line3.style.top = '';
+    line3.style.right = '';
+  } else {
+    mySideNav.style.width = "250px";
+    mySideNav.style.opacity = "1";
+    line1.style.display = 'block';
+    line1.style.transform = 'rotate(45deg)';
+    line1.style.bottom = "10px";
+    line1.style.right = "5px";
+    line2.style.opacity = '0';
+    line3.style.display = 'block';
+    line3.style.transform = 'rotate(-45deg)';
+    line3.style.top = '5px';
+    line3.style.right = '35px';
   }
+}
 
-  open.addEventListener('click', openNav);
-  close.addEventListener('click', closeNav);
-
+open.addEventListener('click', openCloseNav);
