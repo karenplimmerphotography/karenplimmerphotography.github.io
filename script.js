9179feb64d0d8d1063debf16eab9617c10e5c72e
karenplimmerphotography <75963585+karenplimmerphotography@users.noreply.github.com>
2021-09-24 21:29:02 +1000

    mobile menu on about-me page

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+const mySideNav =  document.getElementById("mySideNav");
+const open = document.getElementById("mobile-menu-open");
+const close = document.getElementById("mobile-menu-x");
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+  mySideNav.style.width = "250px";
+  mySideNav.style.opacity = "1";
   }
   /* Set the width of the side navigation to 0 */
   function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    mySideNav.style.width = "0";
   }
+
+  open.addEventListener('click', openNav);
+  close.addEventListener('click', closeNav);
+
