fe9a445e9515abc2b414e104f85f11169d2e03fa
karenplimmerphotography <75963585+karenplimmerphotography@users.noreply.github.com>
2021-09-26 09:47:21 +1000

    add website style guide

--- template.js
+++ template.js
@@ -1,4 +1,6 @@
 const createProjectTemplate = (projectName, day, month, year, module) => {
-    const content = `<article id="${projectName}-article">
-        <div id="${projectName}-header"></div><h4></h4><ul id="${projectName}-description"><li><a href="#">HTML,</a><a href="#>CSS</a> and <a href="#">JS</a> project</li><li></li><li>Created ${day} ${month} ${year}; module ${module}/29</li></ul></article>`
+    const content = `<article id="${projectName}-article"><div id="${projectName}-header"></div><h4>${projectName}</h4><ul id="${projectName}-description"><li><a href="#">HTML,</a><a href="#">CSS</a> and <a href="#">JS</a> project</li><li></li><li>Created ${day} ${month} ${year}; module ${module}/29</li></ul></article>`
+    return content;
 }
+
+createProjectTemplate("website-design", 12, "September", 2021, 3);
